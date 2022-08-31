535cd1eb13c462c77b0600c70040df7ef6e0281e
Nicolly Gabriele Santos Barbosa <99298860+NicollyGab@users.noreply.github.com>
2022-08-31 16:40:56 -0300

    Add files via upload

---
+++ Mundo invertido/assets/js/firebase/app.js
@@ -0,0 +1,19 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCpbjgZo8BupcXvT3kV1i205qecFlVheKc",
+    authDomain: "teste-c3052.firebaseapp.com",
+    projectId: "teste-c3052",
+    storageBucket: "teste-c3052.appspot.com",
+    messagingSenderId: "911657531612",
+    appId: "1:911657531612:web:e3ba9f68a8a1240eac1c19"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+export default app;
